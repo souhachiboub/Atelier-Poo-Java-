53a926f84a25d4919580052fb4548f2991945901
souhachiboub <chiboubsouha1@gmail.com>
2024-11-14 22:04:10 +0100

    feat:add concept of exception

---
+++ src/tn/esprit/gestionzoo/Exception/InvalidAgeException.java
@@ -0,0 +1,9 @@
+package tn.esprit.gestionzoo.Exception;
+
+public class InvalidAgeException extends Exception{
+
+        public InvalidAgeException(String message) {
+            super(message);
+        }
+
+}
---
+++ src/tn/esprit/gestionzoo/Exception/ZooFullException.java
@@ -0,0 +1,11 @@
+package tn.esprit.gestionzoo.Exception;
+
+public class ZooFullException extends Exception{
+    public ZooFullException() {
+        super();
+    }
+
+    public ZooFullException(String message) {
+        super(message);
+    }
+}
